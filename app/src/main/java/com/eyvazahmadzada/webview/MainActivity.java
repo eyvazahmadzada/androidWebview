a91836dcabb8a5bd0f55818e36782de389c3b915
Eyvaz Ahmadzada <eyvaz.ahmedzade.12@gmail.com>
2020-12-31 15:02:18 +0400

    splash screen added

--- app/src/main/java/com/eyvazahmadzada/webview/MainActivity.java
+++ app/src/main/java/com/eyvazahmadzada/webview/MainActivity.java
@@ -1,7 +1,6 @@
 package com.eyvazahmadzada.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
